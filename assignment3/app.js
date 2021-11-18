ec70fed37c06d615501d2343c215bc81100eb256
Roh00t <75230041+Roh00t@users.noreply.github.com>
2021-11-18 21:27:19 +0800

    assign3

--- assignment3/app.js
+++ assignment3/app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/MySongsDB');
 
 // Create schemas to represent the “artist” and “song” objects.
-let Songs = new mongoose.Schema({
+const Songs = new mongoose.Schema({
     title: String,
     album: String,
     duration: String,
@@ -14,7 +14,7 @@ let Songs = new mongoose.Schema({
 
 });
 
-let artists = new mongoose.Schema({
+const artists = new mongoose.Schema({
     name: String,
     debut: Number,
     profilePic: String 
@@ -27,36 +27,36 @@ let artist = mongoose.model('artist', artists);
 
 
 // //Find all songs from the song model.
-// mongoose.Songs.find({});
+Songs.find({});
 
 // //Find song by ID, 61947f05fdbfd6f5bf236680
-// Song.find({_ID:'61947f05fdbfd6f5bf236680'},(error,data) =>{
-//     if(error){
-//         console.log(error);
-//     }else{
-//         console.log(data);
-//     }
+Song.find({_ID:'61947f05fdbfd6f5bf236680'},(error,data) =>{
+    if(error){
+        console.log(error);
+    }else{
+        console.log(data);
+    }
 
-// });
+});
 // //Add a new artist into the collection.
-
+// artists.add()
 
 // //Update the debut year of a specific artist. For eg, update the debut year of Stephanie Sun to 2001.
-// Song.findOneAndUpdate({name:'Stephanie Sun'}, {debut:2001},(error,data) =>{
-//     if(error){
-//         console.log(error);
-//     }else{
-//         console.log(data);
-//     }
+Song.findOneAndUpdate({name:'Stephanie Sun'}, {debut:2001},(error,data) =>{
+    if(error){
+        console.log(error);
+    }else{
+        console.log(data);
+    }
 
-// });
+});
 
 
 // //Delete any artist record by id.
-// artists.delete({_ID:'61948160fdbfd6f5bf236685'},(error) =>{
-//     if(error){
-//         console.log(error);
-//     }
-// });
+artists.delete({_ID:'61948160fdbfd6f5bf236685'},(error) =>{
+    if(error){
+        console.log(error);
+    }
+});
 
 
