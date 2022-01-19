cd16ef32c4b8f754cffeff975e5f1a10bd34ae0a
Roh00t <pandarohit05@gmail.com>
2022-01-20 06:21:08 +0800

    routes and dataservice done

--- assignment4/routes.js
+++ assignment4/routes.js
@@ -9,11 +9,51 @@ router.use(express.urlencoded({
     extended: true
 }));
 
-router.get('/', function (req, res) {
-    res.sendFile(__dirname + "/views/index.html");
+router.post('/timetables', function (req, res) {
+    var data = req.body;
+    var moduleId = res.locals.module._id;
+    var tutorId = res.locals.tutor._id;
+    db.addTimetable(data.day, data.start, data.end, moduleId,tutorId, function (err, timetable) {
+        if (err) {
+            console.log(timetable);
+            res.status(500).send("Unable to add a new timetable");
+        } else {
+            res.status(200).send("Timetable has been successfully added!");
+        }
+
+    })
 });
 
-router.get('/js/*', function(req, res)  {
-    res.sendFile(__dirname+"/views/"+req.originalUrl);
-});
+router.get('/api/timetables', function (req, res) {
+    db.getAllTimetables(function (err, timetables) {
+        if (err) {
+            res.status(500).send("Unable to get all timetables.");
+        } else {
+            res.status(200).send(timetables);
+        }
+    })
+})
+
+router.get('/modules/tutor', function (req, res) {
+    var id = req.params.id;
+    db.getTimetableByTutorId(id, function (err, timetable) {
+        if (err) {
+            res.status(500).send("Unable to find an timetable with this tutor id");
+        } else {
+            res.status(200).send(timetable);
+        }
+    })
+})
+
+router.get('/timetable/:id', function (req, res) {
+    var id = req.params.id;
+    db.getTimetableById(id, function (err, timetable) {
+        if (err) {
+            res.status(500).send("Unable to find an timetable with this id");
+        } else {
+            res.status(200).send(timetable);
+        }
+    })
+})
+
 module.exports = router;
--- assignment4/services/dataservice.js
+++ assignment4/services/dataservice.js
@@ -11,18 +11,58 @@ var database = {
             if (err == null) {
                 console.log("Connected to Mongo DB");
                 //initialize schema values
-                
-
+                moduleSchema = schema({
+                    code: String,
+                    name: String,
+                });
+                timetableSchema = schema({
+                    day: String,
+                    start: String,
+                    end: String,
+                    module: {
+                        type: schema.Types.ObjectId,
+                        ref: 'modules'
+                    },
+                    tutor: {
+                        type: schema.Types.ObjectId,
+                        ref: 'tutors'
+                    }
+                });
+                tutorSchema = schema({
+                    name: String,
+                    office: String
+                });
                 var connection = mongoose.connection;
                 //initialize Models
                 moduleModel = connection.model('module', moduleSchema);
                 timetableModel = connection.model('timetable', timetableSchema);
                 tutorModel = connection.model('tutor', tutorSchema);
-              
+
             } else {
                 console.log("Error connecting to Mongo DB");
             }
         })
+    },
+    addTimetable: function (d, s, e, mid, tid, callback) {
+        var newTimetable = new timetableModel({
+            day: d,
+            start: s,
+            end: e,
+            module: mid,
+            tutor: tid
+        });
+        newTimetable.save(callback);
+    },
+    getAllTimetables: function (callback) {
+        //    eventModel.find({}).populate('organizer').exec(callback);
+        //    eventModel.find({}).populate('module', 'company').exec(callback);
+        timetableModel.find({}).populate('module', 'tutor', ['day', 'start', 'end']).exec(callback);
+    },
+    getTimetableByTutorId: function (id, callback) {
+        tutorModel.findById(id).populate('timetable'['day', 'start', 'end', 'code']);
+    },
+    getTimetableById: function (id, callback) {
+        timetableModel.findById(id).populate('module', 'tutor').exec(callback);
     },
 };
 
